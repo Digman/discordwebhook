eb0c54b5d55fa310a81b8b6f1d86e7074f1a6073
Digman <long2513@gmail.com>
2024-05-16 23:18:38 +0800

    update rate

--- discordwebhook.go
+++ discordwebhook.go
@@ -39,7 +39,7 @@ func SendMessage(url string, message Message) error {
 			return nil
 		case http.StatusTooManyRequests:
 			// Rate limit exceeded, retry after backoff duration
-			resetAfter := resp.Header.Get("Reset-After")
+			resetAfter := resp.Header.Get("Retry-After")
 			parsedAfter, err := strconv.ParseFloat(resetAfter, 64)
 			if err != nil {
 				return err
